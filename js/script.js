a1693689242dd2c55e3860333cc45b2ab33ad28c
jforajter <47546199+jforajter@users.noreply.github.com>
2019-04-16 18:54:30 -0500

    trying to fix map

---
+++ js/script.js
@@ -0,0 +1,6 @@
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 36.0544, lng: -112.1401},
+          zoom: 8
+        });
+      }
