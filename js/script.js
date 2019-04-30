ef706cc1d24de399ec0b94563243aa02376f2ede
jforajter <47546199+jforajter@users.noreply.github.com>
2019-04-30 16:44:24 -0500

    still fighting to get js working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-      function initMap() {
+      var map;
+	  function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: 36.0544, lng: -112.1401},
           zoom: 8
         });
+		
+		var marker = new google.maps.Marker({
+		  position: tower,
+		  map: map,
+		  animation: google.maps.Animation.BOUNCE,
+		  icon: 'images/marker.png'
+		  });
+
+		  
+		marker.addListener('click', function() {
+		infowindow.open(map, marker);
+	});
       }
